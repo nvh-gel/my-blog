b56b51e0c15c1067dad567183f1e854b5cb2a74e
nvh-gel <nvhien2703@outlook.com>
2023-05-15 18:49:47 +0700

    implement intro

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,11 +1,28 @@
+import { faBookOpen, faCode, faGamepad, faRoute } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FC } from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import './intro.scss';
 
 interface IntroProps { }
 
-const Intro: FC<IntroProps> = () => (
-   <>
-      Intro Component
-   </>
-);
+const Intro: FC<IntroProps> = () => {
+   const image = 'https://picsum.photos/1920/1080';
+
+   return (
+      <div className="intro" style={{ backgroundImage: `url(${image})` }}>
+         <Container>
+            <Row className='intro-button'>
+               <Col><FontAwesomeIcon icon={faRoute} className='intro-icon' /><br />Travel</Col>
+               <Col><FontAwesomeIcon icon={faBookOpen} className='intro-icon' /><br />Reading</Col>
+               <Col><FontAwesomeIcon icon={faCode} className='intro-icon' /><br />Programing</Col>
+               <Col><FontAwesomeIcon icon={faGamepad} className='intro-icon' /><br />Gaming</Col>
+            </Row>
+         </Container>
+      </div >
+   );
+}
 
 export default Intro;
