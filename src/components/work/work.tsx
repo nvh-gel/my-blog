f9a9edb64dd0103fc00ef5b261f25b60ea4d3db2
nvh-gel <nvhien2703@outlook.com>
2023-05-23 12:04:53 +0700

    implement recent works

--- src/components/work/work.tsx
+++ src/components/work/work.tsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
 import Image from "react-bootstrap/esm/Image";
 import Row from "react-bootstrap/esm/Row";
 import WorkData from "../../interface/work";
-import Card from "react-bootstrap/esm/Card";
+import "./work.scss";
 
 interface WorkProps {
   works: WorkData[];
@@ -16,25 +17,53 @@ const Work: FC<WorkProps> = (props) => {
       <div className="headline">
         <p>Recent Works</p>
       </div>
-      <Row>
-        <Col>
-          <div className="triangle" />
-        </Col>
-      </Row>
-      {props.works.map((work: WorkData) => (
-        <Row key={work.id}>
-          <Col sm={2}>
-            <FontAwesomeIcon icon={work.icon} />
-          </Col>
-          <Col sm={9}>
-            <Card>
-              <h3>{work.title}</h3>
-              <Image src={work.img} />
-              <p>{work.desc}</p>
-            </Card>
+      <Container>
+        <Row>
+          <Col>
+            <div className="triangle" />
           </Col>
         </Row>
-      ))}
+        {props.works.map((work: WorkData) => (
+          <Row key={work.id} className="work-list">
+            <Container>
+              <Row className="work-overview">
+                <Col sm={1}>
+                  <div className="work-bookmark">
+                    <FontAwesomeIcon icon={work.icon} />
+                  </div>
+                </Col>
+                <Col sm={2}>
+                  <div className="work-content">
+                    <div className="tag">{work.cat}</div>
+                    <h3>{work.title}</h3>
+                  </div>
+                </Col>
+                <Col sm={2}>
+                  <div className="work-content">
+                    <h3>Date</h3>
+                    <p>{work.date.toDateString()}</p>
+                  </div>
+                </Col>
+                <Col sm={4}>
+                  <div className="work-content">
+                    <p>{work.content.substring(0, 100)}...</p>
+                  </div>
+                </Col>
+                <Col sm={3}>
+                  <div className="work-thumbnail">
+                    <Image src={work.img} />
+                  </div>
+                </Col>
+              </Row>
+              <Row className="work-full">
+                <Col sm={12}>
+                  <p>{work.content}</p>
+                </Col>
+              </Row>
+            </Container>
+          </Row>
+        ))}
+      </Container>
     </div>
   );
 };
--- src/interface/work.tsx
+++ src/interface/work.tsx
@@ -3,9 +3,9 @@ import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 export default interface WorkData {
   id: number;
   title: string;
-  desc: string;
-  full: string;
+  content: string;
   img: string;
   date: Date;
   icon: IconDefinition;
+  cat: string;
 }
